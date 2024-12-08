39b6073ad19413a71b94b2f1e8e2053d0381b73b
khatchik <khatchik@kth.se>
2024-12-08 16:02:42 +0100

    the male man Khatchik Shahinyan .

---
+++ src/main/java/com/khatchshah/onetomanyjpa/MyMain.java
@@ -0,0 +1,16 @@
+package com.khatchshah.onetomanyjpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyMain implements CommandLineRunner {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("\n\tthe male man Khatchik Shahinyan .\t\n");
+    }
+}
---
+++ src/test/java/com/khatchshah/onetomanyjpa/OneToManyJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khatchshah.onetomanyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
